11564880fd0dbd6d7b60db74fd5f6835ab8ca754
Jessica Deryckere <jderycke@andrew.cmu.edu>
2023-07-02 21:45:58 -0400

    counts and table names

---
+++ dump_raw_counts.sql
@@ -0,0 +1,8 @@
+SELECT 'bike_data' AS bike_data, COUNT(*) AS row_count FROM bike_data;
+SELECT 'central_park_weather' AS central_park_weather, COUNT(*) AS row_count FROM central_park_weather;
+SELECT 'daily_citi_bike_trip_counts_and_weather' AS daily_citi_bike_trip_counts_and_weather, COUNT(*) AS row_count FROM daily_citi_bike_trip_counts_and_weather;
+SELECT 'fhv_bases' AS fhv_bases, COUNT(*) AS row_count FROM fhv_bases;
+SELECT 'fhv_tripdata' AS fhv_tripdata, COUNT(*) AS row_count FROM fhv_tripdata;
+SELECT 'fhvhv_tripdata' AS fhvhv_tripdata, COUNT(*) AS row_count FROM fhvhv_tripdata;
+SELECT 'green_tripdata' AS green_tripdata, COUNT(*) AS row_count FROM green_tripdata;
+SELECT 'yellow_tripdata' AS yellow_tripdata, COUNT(*) AS row_count FROM yellow_tripdata;
